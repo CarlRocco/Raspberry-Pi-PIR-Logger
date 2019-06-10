21888057b80299f6374eda993e2f79707adf10be
Chris Rocco <chris.rocco7@gmail.com>
2019-06-09 21:54:36 -0700

    Moved table rendering logic to client-side. PHP script now encodes the data as JSON, which gets parsed on the front-end

--- ILSDataAJAX.php
+++ ILSDataAJAX.php
@@ -1,47 +1,25 @@
 <?php
+	ini_set('display_errors', 'On');
+
 	//open the database
-    $db = new PDO('sqlite:PIRLog.db');
-    
-    //query the ILS Data table and get the last entry
-    $result = $db->query("SELECT * FROM ILSData ORDER BY ID DESC LIMIT 1");
-    
-    //populate the variables
-    $dt = "";
-    $ddm = $sdm = $rf = $mod90 = $mod150 = $alarm = 0;
-    foreach($result as $row)
-    {
-		$dt = $row['DATETIME'];
-		$rf = $row['RF'];
-		$ddm = $row['DDM'];
-		$sdm = $row['SDM'];
-		$mod90 = $row['MOD90'];
-		$mod150 = $row['MOD150'];
-		$alarm = $row['ALARM'];
-    }
-    
-     // close the database connection
-		$db = NULL;
-		
-		echo "
-			<table border=1 >
-				<tr>
-					<th>Date Time (UTC)</th>
-					<th>DDM</th>
-					<th>RF Lvl</th>
-					<th>SDM</th>
-					<th>90Mod</th>
-					<th>150Mod</th>
-					<th>Alarm</th>
-				</tr>
-				<tr>
-					<td width='200'>$dt</td>
-					<td width='80'>$ddm</td>
-					<td width='80'>$rf</td>
-					<td width='80'>$sdm</td>
-					<td width='80'>$mod90</td>
-					<td width='80'>$mod150</td>
-					<td width='80'>$alarm</td></tr>
-				</tr>
-			</table>
-		";
+	$db = new PDO('sqlite:PIRLog.db');
+	
+	//query the ILS Data table and get the last entry
+	$result = $db->query("SELECT * FROM ILSData ORDER BY ID DESC LIMIT 1");
+	
+	//populate the variables
+	$row = $result->fetch();
+
+		// close the database connection
+	$db = NULL;
+
+	echo json_encode([
+		'dt' => $row['DATETIME'],
+		'ddm' => $row['DDM'],
+		'rfLevel' => $row['RF'],
+		'sdm' => $row['SDM'],
+		'mod90' => $row['MOD90'],
+		'mod150' => $row['MOD150'],
+		'alarm' => $row['ALARM']
+	]);
 ?>
